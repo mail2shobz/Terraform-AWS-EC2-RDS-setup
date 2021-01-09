95db4e92cbe467eb30c50a3dafc7e499631383a4
drummer shob <mail2shobz@gmail.com>
2021-01-09 16:28:46 +0530

    Update VPC.tf

--- VPC.tf
+++ VPC.tf
@@ -1,32 +1,20 @@
-resource "aws_vpc" "_" {
-  cidr_block = var.vpc_cidr
+module "vpc" {
+  source = "../../modules/vpc"
 
-  enable_dns_support   = var.enable_dns_support
-  enable_dns_hostnames = var.enable_dns_hostnames
-}
+  resource_tag_name = var.resource_tag_name
+  namespace         = var.namespace
+  region            = var.region
 
-resource "aws_internet_gateway" "_" {
-  vpc_id = aws_vpc._.id
-}
+  vpc_cidr = "10.0.0.0/16"
 
-resource "aws_route_table" "_" {
-  vpc_id = aws_vpc._.id
-
-  dynamic "route" {
-    for_each = var.route
-
-    content {
-      cidr_block     = route.value.cidr_block
-      gateway_id     = route.value.gateway_id
-      instance_id    = route.value.instance_id
-      nat_gateway_id = route.value.nat_gateway_id
+  route = [
+    {
+      cidr_block     = "0.0.0.0/0"
+      gateway_id     = module.vpc.gateway_id
+      instance_id    = null
+      nat_gateway_id = null
     }
-  }
-}
+  ]
 
-resource "aws_route_table_association" "_" {
-  count          = length(var.subnet_ids)
-
-  subnet_id      = element(var.subnet_ids, count.index)
-  route_table_id = aws_route_table._.id
+  subnet_ids = module.subnet_ec2.ids
 }
